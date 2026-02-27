40d8a006ccf39204180066aa5fa351a494ce536e
HellHood <szymek0107@gmail.com>
2026-02-27 15:49:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project_P1
+
+Developed with Unreal Engine 5
---
+++ Source/Project_P1/Project_P1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_P1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_P1, "Project_P1" );
